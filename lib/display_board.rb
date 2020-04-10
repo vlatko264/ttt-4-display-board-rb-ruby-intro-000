4b50e5f6610cb7e058b87fc874d6ad165add8992
Vlatko Ploskoski <wiseraven23@yahoo.com>
2020-04-10 20:08:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,25 +1,3 @@
 # Define display_board that accepts a board
 # and prints out the current state.
-def display_board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
 
-def display_board(x)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | x |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-def display_board(O)
-  puts " O |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-end
